eac4277e9be2d124f5d827f78a88304785cbdc8c
Bruno Domingues <brunood07@gmail.com>
2021-02-24 13:09:40 -0300

    challenge box added

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/77869200?s=460&v=4" alt="Bruno Domingues"/>
+            <div>
+                <strong>Bruno Domingues da Silva</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    )
+}
