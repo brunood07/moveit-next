e3f884d3f38bf4ec6c1673924027624872dddd50
Bruno Domingues <brunood07@gmail.com>
2021-02-25 21:49:59 -0300

    exercises add and buttons

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://avatars.githubusercontent.com/u/77869200?s=460&v=4" alt="Bruno Domingues"/>
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Bruno Domingues da Silva</strong>
                 <p>
                     <img src="icons/level.svg" alt="level"/>
-                    Level 1
+                    Level { level }
                 </p>
             </div>
         </div>
